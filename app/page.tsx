550b5600a71dc18c026e1fe12ba55c0880536d64
Amool <amoolkumar10@gmail.com>
2025-08-08 12:58:18 +0530

    Add pages for login/register and connect with api

--- app/page.tsx
+++ app/page.tsx
@@ -1,80 +1,14 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 export default function Home() {
-  const [email, setEmail] = useState<string>();
-  const [password, setPassword] = useState<string>();
-
   useEffect(() => {
-    const getData = async () => {
-      const data = await fetch("/api/login", {
-        method: "POST",
-        body: JSON.stringify({
-          email: "amool@gmail.com",
-          password: "1234@1234",
-        }),
-      });
-      console.log(await data.json());
-    };
-    // getData();
+
   }, []);
 
-  const login = async () => {
-    const data = await fetch("/api/login", {
-      method: "POST",
-      body: JSON.stringify({
-        email: email,
-        password: password,
-      }),
-    });
-
-    console.log(await data.json());
-  };
-
-  const register = async () => {
-    const data = await fetch("/api/register", {
-      method: "POST",
-      body: JSON.stringify({
-        email: email,
-        password: password,
-      }),
-    });
-
-    console.log(await data.json());
-  };
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1>Hello next js</h1>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          gap: 13,
-        }}
-      >
-        <input
-          placeholder="Enter your email"
-          name="email"
-          type="email"
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
 
-        <input
-          placeholder="Enter your password"
-          name="password"
-          type="password"
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-
-        <button onClick={login}>Login</button>
-        <button onClick={register}>Register</button>
-      </div>
     </div>
   );
 }
---
+++ components/login-form.tsx
@@ -0,0 +1,134 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+import { toast } from "sonner";
+
+type LoginFormProps = React.ComponentProps<"div"> & {
+  pathname?: string;
+};
+
+export function LoginForm({ className, pathname, ...props }: LoginFormProps) {
+  const isLoginPage = pathname === "/login";
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
+
+  const login = async () => {
+    const data = await fetch("/api/login", {
+      method: "POST",
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    });
+
+    const { message } = await data.json();
+    if (data.status === 200) {
+      toast.success(message);
+    } else {
+      toast.error(message);
+    }
+  };
+
+  const register = async () => {
+    const data = await fetch("/api/register", {
+      method: "POST",
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    });
+
+    // console.log(await data.json(), data.status);
+    const { message } = await data.json();
+    if (data.status === 201) {
+      toast.success(message);
+    } else {
+      toast.error(message);
+    }
+  };
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  {/* <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a> */}
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  required
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button
+                  onClick={() => {
+                    isLoginPage ? login() : register();
+                  }}
+                  type="button"
+                  className="w-full"
+                >
+                  {isLoginPage ? "Login" : "Register"}
+                </Button>
+                {/* <Button variant="outline" className="w-full">
+                  Login with Google
+                </Button> */}
+              </div>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              {isLoginPage ? (
+                <>
+                  Don&apos;t have an account?{" "}
+                  <a href="/register" className="underline underline-offset-4">
+                    Sign up
+                  </a>
+                </>
+              ) : (
+                <>
+                  Already have an account?{" "}
+                  <a href="/login" className="underline underline-offset-4">
+                    Log in
+                  </a>
+                </>
+              )}
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
